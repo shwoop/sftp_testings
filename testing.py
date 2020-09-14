832c8caaa34c196f54e4bbc409ac35b7b4c62f1b
shwoop <ferguson.alistair@gmail.com>
2020-09-14 15:05:04 +0100

    Init

---
+++ testing.py
@@ -0,0 +1,26 @@
+import stat
+from paramiko import Transport
+from paramiko.sftp_client import SFTPClient
+
+
+def main():
+    t = Transport(('127.0.0.1', 2222))
+    t.connect(None, 'foo', 'pass')
+
+    sftp = SFTPClient.from_transport(t)
+    loc = 'upload'
+    items_1 = []
+    items_2 = []
+    files = []
+    for item in sftp.listdir(loc):
+        items_1.append(item)
+    for item in sftp.listdir_iter(loc):
+        items_2.append(item)
+        if stat.S_IFMT(item.st_mode) != stat.S_IFDIR:
+            files.append(item)
+    assert len(items_1) == len(items_2)
+    print(f'{len(items_2)} items listed but only {len(files)} files')
+
+
+if __name__ == '__main__':
+    main()
